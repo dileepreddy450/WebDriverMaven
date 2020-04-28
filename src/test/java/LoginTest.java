b4a82c1b80626191ec39bd600bcbb0a68ccde014
dileep <dileepreddy450@gmail.com>
2020-04-28 00:38:13 -0500

    comitting the changes

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -20,7 +20,7 @@ public class LoginTest {
 		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
 		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
-		
+		driver.getTitle();
 	}
 	
 	@AfterSuite
