06d045a07edb3348a896e3b787f41e775d396304
dileep <dileepreddy450@gmail.com>
2020-04-28 00:22:28 -0500

    Initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+
+	public static WebDriver driver;
+    @BeforeSuite
+	public void setup() {
+		driver = new ChromeDriver();
+		
+	}
+	@Test
+	public void doLogin(){
+		
+		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+		
+	}
+	
+	@AfterSuite
+	public void teardown() {
+		
+		driver.quit();
+	}
+	
+	
+	
+	
+}
