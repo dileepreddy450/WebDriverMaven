fb8caa3ed5c8135455b1aa237073abe902a23318
dileep <dileepreddy450@gmail.com>
2020-04-28 22:59:05 -0500

    my second jenkins commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +22,7 @@ public class LoginTest {
 		
 		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
 		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
 		driver.getTitle();
 	}
